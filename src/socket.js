cc8485035cdd339c559f86c736c6b9b1fa810e4a
Kenneth Rofuli <rofuli.kj@gmail.com>
2025-08-24 02:06:41 -0700

    fixing websocket

--- src/socket.js
+++ src/socket.js
@@ -2,6 +2,11 @@ import { io } from 'socket.io-client';
 
 //const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:3000';
 const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'https://notesharing-backend-unj3.onrender.com/api';
-const socket = io(SOCKET_URL, { autoConnect: true });
+
+const socket = io(SOCKET_URL, 
+    { 
+        autoConnect: true, 
+        transports: ['websocket', 'polling']
+    });
 
 export default socket;
