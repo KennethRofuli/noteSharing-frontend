c924be31f345d8c9e2c18238e66666ca6df71fce
Kenneth Rofuli <rofuli.kj@gmail.com>
2025-08-24 01:03:55 -0700

    deployed on vercel

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:3000/api'
+    baseURL: 'https://notesharing-backend-unj3.onrender.com/api'
 });
 
 // Add JWT token to request headers if available
